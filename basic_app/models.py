e3c55cf141b816663c1c3c16295b585472adb86b
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-18 14:21:13 -0800

    finished part 1 of detail view and list view

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, PositiveBigIntegerField
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+
+class School(models.Model):
+    name = CharField(max_length=256)
+    principal = CharField(max_length=256)
+    location = CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = CharField(max_length=256)
+    age = PositiveBigIntegerField()
+    school = ForeignKey(School, related_name="students", on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
--- basic_app/views.py
+++ basic_app/views.py
@@ -1,11 +1,13 @@
+from typing import List
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from models import School, Student
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class SchoolListView(ListView):
+    model = School
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["injectme"] = "BASIC Injection"
-        return context
+
+class SchoolDetailView(DetailView):
+    model = School
+    template_name = "basic_app/school_detail.html"
